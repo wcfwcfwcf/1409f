dcf8edf37004671c4e157b26f2bcb495256bfa53
lll <15210485120@163.com>
2016-11-23 11:29:46 +0800

    s

--- single/src/single/Test.java
+++ single/src/single/Test.java
@@ -20,6 +20,29 @@ public class Test {
 	}
 
 }
+
+
+/**
+ * @作者：Administrator
+ * @时间：下午1:31:41
+ */xiangyang
+public class Test {
+
+	private static Test test;
+	private Test(){}
+	public static Test getTest(){
+		if(test == null)
+		{
+			test = new Test();
+		}
+		return test;
+	}
+
+}
+
+
+
+
 /**
  * @作者：Administrator
  * @时间：下午1:31:41
